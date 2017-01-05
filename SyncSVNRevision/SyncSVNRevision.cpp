4fd1b81e883d87a88786a3291cf16b3870740667
windpenguin <windpenguin@gmail.com>
2017-01-05 16:08:41 +0800

    add code

--- SyncSVNRevision/SyncSVNRevision.cpp
+++ SyncSVNRevision/SyncSVNRevision.cpp
@@ -2,10 +2,28 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include "svn_revision.h"
+using namespace std;
+
 
 
 int main()
 {
+#ifdef SVN_REVISION_WCREV
+	cout << SVN_REVISION_WCREV << endl;
+#endif
+
+#ifdef SVN_REVISION_WCDATE
+	cout << SVN_REVISION_WCDATE << endl;
+#endif
+
+#ifdef SVN_REVISION_WCMODS
+	cout << SVN_REVISION_WCMODS << endl;
+#endif
+
+	system("pause");
+
     return 0;
 }
 
---
+++ SyncSVNRevision/svn_revision_template.h
@@ -0,0 +1,8 @@
+#ifndef _SVN_REVISION_H_
+#define _SVN_REVISION_H_   
+
+#define SVN_REVISION_WCREV	 	"Highest committed revision number: $WCREV$"
+#define SVN_REVISION_WCDATE		"Date of highest committed revision: $WCDATE$"
+#define SVN_REVISION_WCMODS		"$WCMODS?Local modifications found:Local modifications not found$"
+
+#endif
